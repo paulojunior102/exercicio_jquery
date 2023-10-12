16d32195a28b24fd47a7ca01e36696bed15e0bdc
paulojúnioe102 <juniormotapaulo@gmail.com>
2023-10-12 20:03:37 -0300

    execicio_jquery

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  $("form").on("submit", function (e) {
+    e.preventDefault();
+
+    const novaTarefa = $("#nova-tarefa").val();
+    const listaTarefa = $("<li></li>");
+    $(listaTarefa).text(novaTarefa);
+    $(listaTarefa).appendTo("ul");
+
+    $("#nova-tarefa").val("");
+  });
+
+  $("ul").on("click", "li", function () {
+    $(this).addClass("tarefa-concluida");
+  });
+});
